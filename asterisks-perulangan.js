6b474474cd6ab5c5effb23e6e72a65d5bacfd07d
eltim12 <eltim2000@gmail.com>
2018-09-30 23:57:45 +0700

    rabu

---
+++ asterisks-perulangan.js
@@ -0,0 +1,35 @@
+
+//1
+
+var baris1 = 5
+while(baris1 > 0) {
+  console.log(' * ')
+  baris1--
+}
+
+
+//2
+
+
+var bintang = 5;
+var kolom = 0;
+for(kolom; kolom < bintang; kolom++) {
+  var jumlah='';
+   for(var baris = 0; baris < bintang; baris++) {
+    jumlah += ' * ';
+  } console.log(jumlah)
+} 
+
+
+
+var bintang = 6;
+
+for(var kolom = 0; kolom < bintang; kolom++) {
+  var jumlah='';
+   for(var baris = 0; baris <= kolom; baris++) {
+    jumlah += ' * ';
+  } console.log(jumlah)
+}
+
+
+
---
+++ perulangan.js
@@ -0,0 +1,86 @@
+
+console.log("nomor 1");
+
+var flag = 2;
+while(flag <= 20 ) {
+  console.log( flag++ + " I love cooding")
+flag++
+}
+
+console.log("looping kedua")
+var flag2 = 20;
+while(flag2 >= 2 ) {
+  console.log( flag2-- + " I will become fullstack developer");
+flag2--
+}
+
+console.log("nomor 2");
+
+
+var forlop = 1;
+while(forlop <= 20) 
+  console.log(forlop + " I love coding "); 
+  forlop++; 
+}
+
+var forlop2 = 20;
+while(forlop2 >= 1) 
+  console.log(forlop2 + " I will become fullstack developer ");
+  forlop2 --
+}
+
+console.log("nomor 3");
+
+console.log("problem 1");
+
+var deret100 = 1;
+while(deret100 <= 100) {  
+  console.log('deret ke-' + deret100);  
+  deret100++;  
+}
+
+ console.log("problem 2");
+
+ for(var deret = 1; deret <= 100; deret += 1)
+ {
+   if (deret % 2 ){
+     console.log( deret + " ganjil")
+   } else {
+   console.log (deret +" genap")
+ }
+ }
+
+ console.log("kelipatan 3 pertambahan 2");
+
+
+ for(var deret = 1; deret <= 100; deret += 2)
+ {
+   if (deret % 3 ==0){
+     console.log( deret + " kelipatan 3")
+   } else {
+   console.log (deret +"  ")
+ }
+ }
+
+ console.log("kelipatan 6 pertambahan 5");
+
+ for(var deret = 1; deret <= 100; deret += 5)
+ {
+   if (deret % 6 ==0){
+     console.log( deret + " kelipatan 6")
+   } else {
+   console.log (deret +"  ")
+ }
+ }
+
+ console.log("kelipatan 10 pertambahan 9");
+
+
+ for(var deret = 1; deret <= 100; deret += 9)
+ {
+   if (deret % 10 ==0){
+     console.log( deret + " kelipatan 10")
+   } else {
+   console.log (deret +"  ")
+ }
+ }
